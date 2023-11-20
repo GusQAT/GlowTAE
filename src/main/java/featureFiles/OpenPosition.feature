999fbc171d92a3c8e480eb1a710d8724a3ac3f0f
Gus <gustavo.gp@globant.com>
2023-11-20 17:05:15 -0500

    Support by India

--- src/main/java/featureFiles/OpenPosition.feature
+++ src/main/java/featureFiles/OpenPosition.feature
@@ -7,7 +7,7 @@ Feature:  Initialize browser instance and go to Citrix
   @tag2
   Scenario Outline:
     Given "Acceder" welcome text is displayed on Citrix page
-    When I add <username> and <password>
+    When I add "username" and "password"
     Then Show password on the screen
     And Click on Google Next button
 
--- src/main/java/stepDefinitions/CitrixLoginStep.java
+++ src/main/java/stepDefinitions/CitrixLoginStep.java
@@ -15,45 +15,68 @@ public class CitrixLoginStep{
     WebDriver driver;
     pageObjects.CitrixLogin landingPage;
 
-    @Given("go to Citrix page")
-    public void goToCitrixPage() throws IOException
-    {
-        driver = initializeDriver();
-    }
+//    @Given("go to Citrix page")
+//    public void go_to_citrix_page()  throws IOException
+//    {
+//        driver = initializeDriver();
+//    }
 
-    @Given("{String} welcome text is displayed on Citrix page")
-    public void landedOnCitrixPage(String element)
-    {
+//    @Given("{string} welcome text is displayed on Citrix page")
+//    public void welcome_text_is_displayed_on_citrix_page(String element)
+//    {
+//        landingPage = new pageObjects.CitrixLogin(driver);
+//
+//        String getGoogleConfirmation = landingPage.getConfirmationGooglePage();
+//        Assert.assertTrue(getGoogleConfirmation.toLowerCase().contains(element));
+//    }
+
+//    @When("I add {string} and {string}")
+//    public void i_add_and(String username, String password) {
+//        AbstractComponent callM = new AbstractComponent(driver);
+//
+////        String loginEmail = callM.callData("email");
+//        landingPage.inputEmailAddressGoogle(username);
+//        landingPage.clickOnSiguienteButton();
+//        landingPage.insertPasswordGoogle(password);
+//    }
+
+//    @Then("Show password on the screen")
+//    public void showPasswordOnTheScreen()
+//    {
+//        landingPage.seePassword();
+//    }
+//
+//    @And("Click on Google Next button")
+//    public void clickOnGoogleNextButton()
+//    {
+//        landingPage.googleNextbutton();
+//    }
+@Given("go to Citrix page")
+public void go_to_citrix_page() throws IOException {
+    driver = initializeDriver();
+}
+    @Given("{string} welcome text is displayed on Citrix page")
+    public void welcome_text_is_displayed_on_citrix_page(String element) {
         landingPage = new pageObjects.CitrixLogin(driver);
 
         String getGoogleConfirmation = landingPage.getConfirmationGooglePage();
         Assert.assertTrue(getGoogleConfirmation.toLowerCase().contains(element));
     }
-
-    @When("^I add (.+) and (.+)$") // Regular Expression    //    ^ is called caret symbol
-    public void addUsernameAndPassword(String username, String password)
-    {
+    @When("I add {string} and {string}")
+    public void i_add_and(String username, String password) {
         AbstractComponent callM = new AbstractComponent(driver);
 
 //        String loginEmail = callM.callData("email");
-
         landingPage.inputEmailAddressGoogle(username);
-
         landingPage.clickOnSiguienteButton();
-
         landingPage.insertPasswordGoogle(password);
     }
-
     @Then("Show password on the screen")
-    public void showPasswordOnTheScreen()
-    {
+    public void show_password_on_the_screen() {
         landingPage.seePassword();
     }
-
-    @And("Click on Google Next button")
-    public void clickOnGoogleNextButton()
-    {
+    @Then("Click on Google Next button")
+    public void click_on_google_next_button() {
         landingPage.googleNextbutton();
     }
-
 }
